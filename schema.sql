edbd8812b27c9c4445593eb073b4bf372d6d1bc0
edyi8001 <114195211+edyi8001@users.noreply.github.com>
2022-11-23 16:43:01 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,74 @@
+-- Creating tables for PH-EmployeeDB
+
+-- Create "departments" table
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+-- Check create/import for "departments"
+SELECT * FROM departments;
+
+-- Create "employees" table
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+-- Check create/import for "employees"
+SELECT * FROM employees;
+
+-- Create "managers" table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+-- Check create/import for "managers"
+SELECT * FROM dept_manager;
+
+-- Create "salaries" table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+  	salary INT NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no)
+);
+-- Check create/import for "salaries"
+SELECT * FROM salaries;
+
+-- Create "dept_emp" table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+-- Check create/import for "dept_emp"
+SELECT * FROM dept_emp;
+
+-- Create "titles" table
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+-- Check create/import for "titles"
+SELECT * FROM titles;
